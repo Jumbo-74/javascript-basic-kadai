2288a92295c6c014947d1603ace89f1834aaec8b
Jumbo-74 <tcue.michihisa.74@gmail.com>
2025-03-20 19:06:05 +0900

    Delete kadai_014 directory

--- kadai_014/kadai_014/square.js
+++
@@ -1,6 +0,0 @@
-
-const multipluer = (num) => {
-    return num ** 2;
-    console.log(num);
-}
-console.log(multipluer(10));
--- kadai_014/square.js
+++
@@ -1,5 +0,0 @@
-
-const multipluer = (num) => {
-    console.log(num ** 2);
-}
-multipluer(10);
