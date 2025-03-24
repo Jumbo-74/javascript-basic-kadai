e6aa8249ba22072b4cd8dfc783fd27ee89ce4c1a
Jumbo-74 <tcue.michihisa.74@gmail.com>
2025-03-24 12:13:36 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const outputBtn = document.getElementById('btn');
+outputBtn.addEventListener('click', () => {
+    const text = document.getElementById('text');
+    text.textContent = 'ボタンをクリックしました';
+});
